b7d56b49cb0c1ca085f91c9a72414c0b3618ce74
User <User@NNRPC1E1X90.npr.nornick.ru>
2022-06-19 20:03:08 +0700

    ver 0.1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import {createOffers} from './data.js';
+import './popup.js';
 
 createOffers();
+
---
+++ js/popup.js
@@ -0,0 +1,41 @@
+import {createOffers} from './data.js';
+
+const offerTypes = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalow: 'Бунгало',
+  hotel: 'Отель'
+};
+
+
+const insertBlock = document.querySelector('#map-canvas'); //сюда вставляем
+
+const offerTemplate = document.querySelector('#card')
+  .content
+  .querySelector('.popup'); //фрагмент с содержимым темплейта
+
+
+const similarOffers = createOffers();
+const offerElement = offerTemplate.cloneNode(true);
+offerElement.querySelector('.popup__title').textContent = similarOffers[0].offer.title;
+offerElement.querySelector('.popup__text--address').textContent = similarOffers[0].offer.address;
+offerElement.querySelector('.popup__text--price').textContent = similarOffers[0].offer.price + ' ₽/ночь';
+offerElement.querySelector('.popup__type').textContent = offerTypes.similarOffers[0].offer.type; //вывести тип жилья сопоставив с подписями
+offerElement.querySelector('.popup__text--capacity').textContent = similarOffers[0].offer.rooms + ' комнаты для' + similarOffers[0].offer.guests + ' гостей';
+offerElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + similarOffers[0].offer.checkin + ' , выезд до' + similarOffers[0].offer.checkout;
+offerElement.querySelector('.popup__features').innerHTML = similarOffers[0].offer.title; //список доступных удобств
+offerElement.querySelector('.popup__description').textContent = similarOffers[0].offer.description;
+offerElement.querySelector('.popup__photos').textContent = similarOffers[0].offer.title; //все фотографии из списка в атрибут src
+offerElement.querySelector('.popup__avatar').textContent = similarOffers[0].offer.title; //значение атрибута src
+
+//предусмотреть, что данных не хватает (блок скрывается)
+
+
+
+
+
+
+insertBlock.appendChild(offerElement);
+
+
