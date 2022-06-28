1d53cd9defd2269e4713e408da109f0739dcc3eb
wrst <gbpleq1@gmail.com>
2022-06-28 22:04:45 +0700

    ver 0.1

---
+++ js/user-form.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('.ad-form');
+const pristine = new Pristine(form);
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  const isValid = pristine.validate();
+  if (isValid) {
+    console.log('Можно отправлять');
+  } else {
+    console.log('Форма невалидна');
+  }
+});
