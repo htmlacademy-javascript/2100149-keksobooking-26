5d3c27befd46d72ee0b18eaed3c9f03bbf0c8959
User <User@NNRPC1E1X90.npr.nornick.ru>
2022-07-11 18:16:49 +0700

    дичь

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ import {createOffers} from './data.js';
 import {insertOffer} from './popup.js';
 import {getRandomPositiveInteger} from './util.js';
 import './user-form-validate.js';
+import { activateForm, deactivateForm } from './user-form.js';
+
+deactivateForm();
+activateForm();
 
 const offers = createOffers();
 const randomIndex = getRandomPositiveInteger(0,9);
