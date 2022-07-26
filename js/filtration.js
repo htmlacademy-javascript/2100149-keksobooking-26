74edfb734d93f901265eb7390b68d33250db7191
wrst <gbpleq1@gmail.com>
2022-07-26 07:23:58 +0700

    module 12 with changes

--- js/filtration.js
+++ js/filtration.js
@@ -1,15 +1,19 @@
 import { mapFiltersForm } from './user-form.js';
 
 const DEFAULT_VALUE = 'any';
+const Prices = {
+  MIN_PRICE: 10000,
+  MAX_PRICE: 50000
+};
 
 const filterPrice = (selectedPrice, offerPrice) => {
   switch (selectedPrice) {
     case 'middle':
-      return 10000 <= offerPrice && offerPrice <= 50000;
+      return Prices.MIN_PRICE <= offerPrice && offerPrice <= Prices.MAX_PRICE;
     case 'low':
-      return offerPrice <= 10000;
+      return offerPrice <= Prices.MIN_PRICE;
     case 'high':
-      return offerPrice >= 50000;
+      return offerPrice >= Prices.MAX_PRICE;
     default:
       return true;
   }
@@ -19,11 +23,8 @@ const filterFeatures = (offerFeatures) => {
   const checkedFeaturesList = document.querySelectorAll('[name=features]:checked');
   const checkedFeatures = [];
   checkedFeaturesList.forEach((element) => checkedFeatures.push(element.value));
-  if (checkedFeatures.length !== 0) {
-    if (offerFeatures) {
-      return checkedFeatures.every((element) => offerFeatures.includes(element));
-    }
-    return false;
+  if (checkedFeatures.length) {
+    return (offerFeatures) ? checkedFeatures.every((element) => offerFeatures.includes(element)) : 0;
   }
   return true;
 };
